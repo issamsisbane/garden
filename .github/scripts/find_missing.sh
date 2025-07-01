3fcf5eb2f80fc96b122df4d8fbeeca1585530f5a
Issam <issam.sisbane@gmail.com>
2025-07-01 23:09:44 +0200

    fix: update find_missing script to use path and not file name

--- .github/scripts/find_missing.sh
+++ .github/scripts/find_missing.sh
@@ -8,17 +8,19 @@ for DIR in "${DIRECTORIES[@]}"; do
     continue
   fi
 
-  find "$DIR" -type f \( -name "*.fr.md" -o -name "*.en.md" \) -print0 | while IFS= read -r -d '' FILE; do
-    if [[ "$FILE" == *.fr.md ]]; then
-      OTHER_FILE="${FILE/.fr.md/.en.md}"
+  find "$DIR" -type f -print0 | while IFS= read -r -d '' FILE; do
+    # Vérifier si le chemin contient /fr/ ou /en/
+    if [[ "$FILE" == *"/fr/"* ]]; then
+      OTHER_FILE="${FILE//\/fr\//\/en\/}"
       if [ ! -f "$OTHER_FILE" ]; then
         echo "$FILE"
       fi
-    elif [[ "$FILE" == *.en.md ]]; then
-      OTHER_FILE="${FILE/.en.md/.fr.md}"
+    elif [[ "$FILE" == *"/en/"* ]]; then
+      OTHER_FILE="${FILE//\/en\//\/fr\/}"
       if [ ! -f "$OTHER_FILE" ]; then
         echo "$FILE"
       fi
     fi
   done
 done
+
